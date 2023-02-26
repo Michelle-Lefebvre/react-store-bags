f1396633f37be8e38350c628e1096804561afe94
Michelle Lefebvre <mmldesigns007@gmail.com>
2023-02-25 20:42:19 -0500

    add Banner component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,12 +1,28 @@
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
+import {
+  BannerContainer,
+  BannerContent,
+  BannerDescription,
+  BannerImage,
+  BannerTitle,
+} from "../../styles/bannerStyles";
 
 export default function Banner() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <>
-      <h1>BANNER</h1>
-    </>
+    <BannerContainer>
+      <BannerImage src="/images/banner/banner.png" />
+      <BannerContent>
+        <Typography variant="h6">Huge Collection</Typography>
+        <BannerTitle variant="h2">New Bags</BannerTitle>
+        <BannerDescription variant="subtitle1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </BannerDescription>
+      </BannerContent>
+    </BannerContainer>
   );
 }

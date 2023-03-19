460eb3eec57082cb779523f8e050dfd81d7b0a1b
Michelle Lefebvre <mmldesigns007@gmail.com>
2023-03-18 22:57:49 -0400

    cart update

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,12 @@
-import { Drawer, useMediaQuery, Box, Avatar } from "@mui/material";
+import {
+  Drawer,
+  useMediaQuery,
+  Box,
+  Avatar,
+  Typography,
+  Divider,
+  Paper,
+} from "@mui/material";
 import { useUIContext } from "../../context/uiContext";
 import { Colors } from "../../styles/themeStyles";
 import { useTheme } from "@mui/material/styles";
@@ -9,7 +17,7 @@ export default function Cart() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
-  const cartContent = cart.map((item) => {
+  const cartContent = cart.map((item) => (
     <Box key={item.id}>
       <Box
         display="flex"
@@ -17,10 +25,18 @@ export default function Cart() {
         alignItems="start"
         justifyContent={"space-between"}
       >
-        <Avatar src={item.image} sx={{ width: 96, height: 96 }} />
+        <Avatar src={item.image} sx={{ width: 96, height: 96, mr: 2 }} />
+        <Box display={"flex"} flexDirection={"column"}>
+          <Typography variant="h6">{item.name}</Typography>
+          <Typography variant="subtitle2">{item.description}</Typography>
+        </Box>
+        <Typography variant="body1" justifyContent={"end"}>
+          ${item.price}
+        </Typography>
+        <Divider variant="inset" />
       </Box>
-    </Box>;
-  });
+    </Box>
+  ));
   return (
     <Drawer
       open={showCart}
@@ -30,8 +46,31 @@ export default function Cart() {
         sx: { width: 500, background: Colors.light_gray, borderRadius: 0 },
       }}
     >
-      <h1>My Cart</h1>
-      {cartContent}
+      <Box
+        sx={{ p: 4 }}
+        display="flex"
+        justifyContent={"center"}
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Typography variant="h3" color={Colors.black}>
+          Cart
+        </Typography>
+        <Typography variant="body1" color={Colors.muted}>
+          Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo
+          tempor incididunt ut labore et dolore magna
+        </Typography>
+        <Paper
+          elevation={0}
+          sx={{
+            mt: 2,
+            width: "90%",
+            padding: 4,
+          }}
+        >
+          {cartContent}
+        </Paper>
+      </Box>
     </Drawer>
   );
 }

49261a4b05e56ba328cf9ce4a9580e558cb77d18
Michelle Lefebvre <mmldesigns007@gmail.com>
2023-08-28 21:58:25 -0400

    initial commit react app

---
+++ feedback-app/src/App.js
@@ -0,0 +1,11 @@
+import { dividerClasses } from "@mui/material";
+import React from "react";
+function App() {
+  return (
+    <div className="container">
+      <h1>MML App</h1>
+    </div>
+  );
+}
+
+export default App;
